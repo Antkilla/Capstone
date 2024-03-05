d5cc6d62fa5801d7d5f2b7d5e348fb0effecd264
Antkilla <132300802+Antkilla@users.noreply.github.com>
2024-03-04 20:10:53 -0800

    Made buttons work
    Delete and edit/update button works down

--- client/src/components/DealershipList.jsx
+++ client/src/components/DealershipList.jsx
@@ -2,9 +2,11 @@ import React, { useContext, useEffect }from 'react'
 import { Table, Button } from 'react-bootstrap';
 import DealershipFinder from '../apis/DealershipFinder';
 import { DealershipsContext } from '../context/DealershipsContext';
+import { useNavigate } from 'react-router-dom';
 
 const DealershipList = (props) => {
     const { dealerships, setDealerships } = useContext(DealershipsContext);
+    let Navigate = useNavigate()
   
     useEffect(() => {
       const fetchData = async () => {
@@ -20,6 +22,23 @@ const DealershipList = (props) => {
   
       fetchData();
     }, []); // <-- Dependency array is empty since we only want to run this once
+
+    const handleDelete = async (id) => {
+        try {
+            const response = await DealershipFinder.delete(`/${id}`);
+            setDealerships(
+                dealerships.filter((dealership) => {
+                return dealership.id !== id
+            }));
+            console.log(response);
+        }   catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleUpdate = (id) => {
+        Navigate(`dealership/${id}/update`);
+    }
   
   return (
         <div className="list-group">
@@ -43,10 +62,10 @@ const DealershipList = (props) => {
                                 <td>{"$".repeat(dealership.price_range)}</td>
                                 <td>reviews</td>
                                 <td>
-                                    <Button variant="warning">Update</Button>
+                                    <Button onClick={() => handleUpdate(dealership.id)} className="btn btn-warning">Update</Button>
                                 </td>
                                 <td>
-                                    <Button variant="danger">Delete</Button>
+                                    <Button onClick={() => handleDelete(dealership.id)} className="btn btn-danger">Delete</Button>
                                 </td>
                             </tr>
                         );
